526e3e1ea8c386108c79ea19c61ed2eae820f437
alistairr <alistair.russell.work@gmail.com>
2023-08-07 16:43:27 -0700

    Initial application logic for serving a static home page

--- main.go
+++ main.go
@@ -1,64 +1,51 @@
 // main.go
 package main
+
 import (
-   "github.com/gin-gonic/gin"
-   "gitlab.com/alistairr/issue-tracker/db"
-   "log"
-   "net/http"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/alistairr/spartan/models"
 )
+
 var (
-   ListenAddr = "localhost:8080"
-   RedisAddr = "localhost:6379"
+	router       *gin.Engine
+	ListenAddr   = "localhost:8080"
+	PostgresAddr = "localhost:5432"
 )
 
 func main() {
-    database, err := db.NewDatabase(RedisAddr)
-    if err != nil {
-        log.Fatalf("Failed to connect to redis: %s", err.Error())
-    }
+	// MIGRATIONS
+	models.Migrate()
+	// END MIGRATIONS
 
-    router := gin.Default()
+	router := gin.Default()
 
-    router.POST("/issue", func(c *gin.Context) {
-        var issue Issue
-        if err := c.ShouldBindJSON(&issue); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
-        
-        issueID := database.CreateIssue(&issue)
+	router.Static("/static", "./static")
 
-        c.JSON(http.StatusOK, gin.H{"status": "Created", "id": issueID})
-    })
+	router.LoadHTMLGlob("templates/*")
+	log.Println("Post template load")
 
-    router.GET("/issue/:id", func(c *gin.Context) {
-        issueID := c.Param("id")
-        issue := database.GetIssue(issueID)
+	// ROUTES
+	router.GET("/", Home)
+	router.POST("/", CreateProject)
+	router.GET("/:url", ProjectList)
+	// issue crud
+	router.POST("/:url/issue", Issue)
 
-        c.JSON(http.StatusOK, issue)
-    })
-
-    router.PATCH("/issue/:id", func(c *gin.Context) {
-        issueID := c.Param("id")
-
-        var updatedFields map[string]interface{}
-        if err := c.ShouldBindJSON(&updatedFields); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
-
-        database.UpdateIssue(issueID, updatedFields)
-
-        c.JSON(http.StatusOK, gin.H{"status": "Updated", "id": issueID})
-    })
-
-    router.DELETE("/issue/:id", func(c *gin.Context) {
-        issueID := c.Param("id")
-        database.DeleteIssue(issueID)
-
-        c.JSON(http.StatusOK, gin.H{"status": "Deleted", "id": issueID})
-    })
-
-    router.Run(ListenAddr)
+	// Run server
+	router.Run()
 }
 
+// Render either HTML or JSON depending on 'Accept' header
+func render(c *gin.Context, data gin.H, templateName string) {
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		c.JSON(http.StatusOK, data["payload"])
+	default:
+		c.HTML(http.StatusOK, templateName, data)
+	}
+
+}
