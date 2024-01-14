67d68d604152d505d3fb1ca532ae9b5beb2d4eae
alistairr <alistair.russell.work@gmail.com>
2024-01-14 13:39:17 -0800

    Added dagger for local ci

---
+++ ci/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	// Project to test
+	src := client.Host().Directory(".")
+
+	_, err = client.Container().
+		From("golangci/golangci-lint:v1.55").
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src").
+		WithExec([]string{"golangci-lint", "run", "-v"}).
+		Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = client.Container().
+		From("golang:1.21").
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src").
+		WithExec([]string{"go", "test"}).
+		WithExec([]string{"go", "build"}).
+		Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+}
